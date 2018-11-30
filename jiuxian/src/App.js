ae4d2ff4c739bb913eab00cb0de69ae37b80e970
aaron <jeroo@qq.com>
2018-11-30 12:02:58 +0800

    项目：环境搭建

--- jiuxian/src/App.js
+++ jiuxian/src/App.js
@@ -1,28 +1,136 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+import {Route,NavLink,Redirect,Switch,withRouter} from 'react-router-dom';
+
+import { TabBar } from 'antd-mobile';
+
+//引入ant-design-mobile的样式
+import 'antd-mobile/dist/antd-mobile.css';
+
+import './sass/page.scss';
+
+import {Home} from './components/Home';
+import {List} from './components/List';
+import {Goods} from './components/Goods';
+import {Cart} from './components/Cart';
+import {NotFound} from './components/Page';
+
+// fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+    faHome,
+    faListUl,
+    faShoppingCart,
+    faAssistiveListeningSystems } from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+    faHome,
+    faListUl,
+    faShoppingCart,
+    faAssistiveListeningSystems
+)
+
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+        tabs:[
+            {
+                title:'首页',
+                path:'/home',
+                icon:'home'
+            },
+            {
+                title:'列表',
+                path:'/list',
+                icon:'list-ul'
+            },
+            {
+                title:'详情',
+                path:'/goods',
+                icon:'assistive-listening-systems'
+            },
+            {
+                title:'购物车',
+                path:'/cart',
+                icon:'shopping-cart'
+            }
+        ],
+        currentTab:0
+    }
+  }
+
+  handlerClick(idx,path){
+      this.setState({
+          currentTab:idx
+      });
+
+      //编程式导航
+      //获取history的方式
+      // * 通过Route渲染App
+      // * 利用withRouter高阶组件实现
+      this.props.history.push(path);
+
+  }
+
+  componentWillMount(){
+      //获取hash值
+      let hash = window.location.hash.slice(1);//#list
+
+      //找出对应索引值
+      let currentTab = 0
+      this.state.tabs.some((item,idx)=>{
+          currentTab = idx;
+          return item.path === hash
+      });
+
+      this.setState({
+          currentTab
+      });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="container">
+            <div className="content">
+                <Switch>
+                    <Route path="/home" component={Home} />
+                    <Route path="/list" component={List} />
+                    <Route path="/goods/:id" component={Goods} />
+                    <Route path="/cart" component={Cart} />
+                    <Route path="/404" component={NotFound} />
+                    <Redirect from="/" to="/home" exact/>
+                    <Redirect to="/404"/>
+                </Switch>
+            </div>
+            <TabBar
+                tintColor="#f00"
+                noRenderContent={true}
+                >
+                {
+                    this.state.tabs.map((tab,idx)=>{
+                        return <TabBar.Item
+                            title={tab.title}
+                            key={tab.path}
+                            icon={<FontAwesomeIcon icon={tab.icon}/>}
+                            selectedIcon={<FontAwesomeIcon icon={tab.icon}/>}
+                            selected={this.state.currentTab === idx}
+                            onPress={this.handlerClick.bind(this,idx,tab.path)}
+                            >
+                            {tab.title}
+                            </TabBar.Item>
+                    })
+                }
+                
+            </TabBar>
+        </div>
     );
   }
 }
+
+App = withRouter(App);
 
 export default App;
---
+++ jiuxian/src/components/Home.js
@@ -0,0 +1,93 @@
+import React,{Component} from 'react';
+import axios from 'axios';
+
+import {withRouter} from 'react-router-dom';
+
+import { List,Carousel } from 'antd-mobile';
+const Item = List.Item;
+const Brief = Item.Brief;
+
+class Home extends Component{
+    constructor(){
+        super();
+        this.state = {
+            //轮播图商品
+            ad:[],
+            //列表
+            goodslist:[]
+        }
+
+        this.handlerClick = this.handlerClick.bind(this);
+    }
+    componentWillMount(){console.log(6666)
+		axios.get('/jxapi/m_v1/promote/qgajax.do',{
+			params:{
+				t:Date.now(),
+				pagenum:1,
+				tabnum:1
+			}
+		}).then(res=>{
+			let data = res.data;console.log(data)
+			
+			this.setState({
+				ad:data.killProList.slice(0,4),
+				goodslist:data.killProList.slice(4)
+			});
+		});
+    }
+    handlerClick(goods){
+        //获取history
+        let {history} = this.props;
+        console.log(history);
+        history.push({
+            pathname:'/goods/'+goods.proId,
+            state:goods
+        });
+    }
+    render(){
+        return <div>
+             <Carousel
+                autoplay={true}
+                infinite
+                >
+                {this.state.ad.map(goods => (
+                    <a
+                    key={goods.proId}
+                    href="#"
+                    style={{height:'320px'}}
+                    >
+                    <img
+                        src={goods.proImg}
+                        style={{ width: '100%', height:'200px', verticalAlign: 'top' }}
+                        onLoad={() => {
+                            window.dispatchEvent(new Event('resize'));
+                        }}
+                    />
+                    </a>
+                ))}
+                </Carousel>
+            <List renderHeader={() => '热卖商品'}>
+                {
+                    this.state.goodslist.map(goods=>{
+                        return <Item
+                        thumb={goods.proImg}
+                        arrow="horizontal"
+                        onClick={this.handlerClick.bind(this,goods)}
+                        key={goods.proId}
+                        >
+                        <h4>{goods.proName}</h4>
+                        <p className="price">原价：<del>{goods.proPrice}</del></p>
+                        <p className="price">现价：<span>{goods.proPrice*goods.sellPercent/100}</span></p>
+                        </Item>
+                    })
+                }
+                
+            
+            </List>
+        </div>
+    }
+}
+
+Home = withRouter(Home);
+
+export {Home};
