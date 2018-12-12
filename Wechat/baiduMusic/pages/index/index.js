5890bcaa470b73c45f31a4c27a10aefb90d16940
aaron <jeroo@qq.com>
2018-12-12 15:58:43 +0800

    小程序-百度音乐-轮播图

---
+++ Wechat/baiduMusic/pages/index/index.js
@@ -0,0 +1,37 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    autoplay:true,
+    recommend:[]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    // 热门歌曲
+    wx.request({
+      url: 'https://tingapi.ting.baidu.com/v1/restserver/ting',
+      data: {
+        method: 'baidu.ting.billboard.billList',
+        type: 2,
+        size: 7,
+        offset: 0
+      },
+      success: res => {
+        let data = res.data;
+
+        this.setData({
+          recommend:data.song_list
+        })
+
+
+      }
+    });
+  }
+})
